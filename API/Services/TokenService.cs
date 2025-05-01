a9745ac3cddf09f3bb954715fe80caa77a32b5c5
Turtlegods <strollist@gmail.com>
2025-05-01 18:22:51 +0800

    add Token Service

---
+++ API/Services/TokenService.cs
@@ -0,0 +1,40 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using API.Entities;
+using API.Interfaces;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
+using Microsoft.IdentityModel.Tokens;
+
+namespace API.Services;
+
+public class TokenService(IConfiguration config) : ITokenService
+{
+    public string CreateToken(AppUser user)
+    {
+        var tokenKey = config["TokenKey"]?? throw new Exception("Cannot access tokenKey from appsettings");
+        if (tokenKey.Length < 64){
+            throw new Exception("TokenKey needs to be longer");
+        }
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenKey));
+
+        key.KeyId = tokenKey;
+        var claims = new List<Claim>{
+            new (ClaimTypes.NameIdentifier, user.Id.ToString()),
+        };
+
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+        var tokenDescripter = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(claims),
+            Expires = DateTime.UtcNow.AddDays(7),
+            SigningCredentials =creds,
+        };
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var token = tokenHandler.CreateToken(tokenDescripter);
+        return tokenHandler.WriteToken(token);
+    }
+}
