321cabc96d5402380babbe773d7153c33ff94558
Turtlegods <strollist@gmail.com>
2025-05-01 17:06:10 +0800

    加入LoginDto

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -2,7 +2,7 @@ using System;
 using System.Security.Cryptography;
 using System.Text;
 using API.Data;
-using API.DTO;
+using API.DTOs;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -26,6 +26,22 @@ public class AccountController(DataContext context) : BaseApiController
         };
         context.Users.Add(user);
         await context.SaveChangesAsync();
+        return user;
+    }
+    [HttpPost("login")]// account/login
+    public async Task<ActionResult<AppUser>>Login(LoginDto loginDto)
+    {
+        var user = await context.Users.FirstOrDefaultAsync(x=> 
+            x.UserName == loginDto.Username.ToLower());
+        if(user == null) return Unauthorized("Invalid username");
+
+        using var hmac = new HMACSHA512(user.PasswordSalt);
+        var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+        for(int i = 0; i < computedHash.Length; i++)
+        {
+            if(computedHash[i] != user.PasswordHash[i])
+                return Unauthorized("Invalid password");
+        }
         return user;
     }
     private async Task<bool> UserExists(string username)
