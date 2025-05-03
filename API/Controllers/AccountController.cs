fd75f4b0055da739b6dd96c1239c6526d05c304e
Turtlegods <strollist@gmail.com>
2025-05-03 22:12:17 +0800

    加入token 驗證

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -4,12 +4,13 @@ using System.Text;
 using API.Data;
 using API.DTOs;
 using API.Entities;
+using API.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
-public class AccountController(DataContext context) : BaseApiController
+public class AccountController(DataContext context, ITokenService tokenService) : BaseApiController
 {
     [HttpPost("register")]// account/register
     public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
@@ -29,7 +30,7 @@ public class AccountController(DataContext context) : BaseApiController
         return user;
     }
     [HttpPost("login")]// account/login
-    public async Task<ActionResult<AppUser>>Login(LoginDto loginDto)
+    public async Task<ActionResult<UserDto>>Login(LoginDto loginDto)
     {
         var user = await context.Users.FirstOrDefaultAsync(x=> 
             x.UserName == loginDto.Username.ToLower());
@@ -42,7 +43,11 @@ public class AccountController(DataContext context) : BaseApiController
             if(computedHash[i] != user.PasswordHash[i])
                 return Unauthorized("Invalid password");
         }
-        return user;
+        return new UserDto
+        {
+            Username = user.UserName,
+            Token = tokenService.CreateToken(user)
+        };
     }
     private async Task<bool> UserExists(string username)
     {
