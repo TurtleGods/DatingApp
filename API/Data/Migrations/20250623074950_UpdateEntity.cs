888255a3bb8273923356f37f3cc455e5d51a8a9a
Turtlegods <strollist@gmail.com>
2025-06-23 15:52:42 +0800

    Entity修正

---
+++ API/Data/Migrations/20250623074950_UpdateEntity.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Intersts",
+                table: "Users",
+                newName: "Interests");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Interests",
+                table: "Users",
+                newName: "Intersts");
+        }
+    }
+}
