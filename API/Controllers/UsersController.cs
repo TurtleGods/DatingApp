be744c14ebdd756500cc2852a7df914e597a6cdc
Turtlegods <strollist@gmail.com>
2025-07-03 00:43:20 +0800

    create Action的使用 是說需要回覆Client一個上傳結果的位置

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -68,7 +68,8 @@ public class UsersController(IUserRepository userRepository, IMapper mapper, IPh
         user.Photos.Add(photo);
         if (await userRepository.SaveAllAsync())
         {
-            return mapper.Map<PhotoDto>(photo);
+            return CreatedAtAction(nameof(GetUser),
+            new { username = user.UserName },mapper.Map<PhotoDto>(photo));
         }
         return BadRequest("Problem adding photo");
     }
