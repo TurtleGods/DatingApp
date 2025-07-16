ddc68f141e5683ba066f008fb5192ff936b11a4b
Turtlegods <strollist@gmail.com>
2025-07-16 13:36:26 +0800

    [feat] Update Member info的功能 這次沒有使用map的功能

--- API/Controllers/MembersController.cs
+++ API/Controllers/MembersController.cs
@@ -1,4 +1,6 @@
+using System.Security.Claims;
 using API.Data;
+using API.DTOs;
 using API.Entities;
 using API.Interfaces;
 using Microsoft.AspNetCore.Authorization;
@@ -32,6 +34,26 @@ namespace API.Controllers
         public async Task<ActionResult<IReadOnlyList<Photo>>> GetMemberPhotos(string id)
         {
             return Ok(await memberRepository.GetPhotosForMemberAsync(id));
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateMember(MemberUpdateDto memberUpdateDto)
+        {
+            var memberId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            if (memberId == null) return BadRequest("Oops -no id found in token");
+
+            var member = await memberRepository.GetMemberByIdAsync(memberId);
+            if (member == null) return BadRequest("Counld not get member");
+
+            member.DisplayName = memberUpdateDto.DisplayName ?? member.DisplayName;
+            member.Description = memberUpdateDto.Description ?? member.Description;
+            member.City = memberUpdateDto.City ?? member.City;
+            member.Country = memberUpdateDto.Country ?? member.Country;
+
+            if (await memberRepository.SaveAllAsync()) return NoContent();
+
+            return BadRequest("Fail to update member");
         }
         
 
