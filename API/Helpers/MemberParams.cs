333b25d466b54fb24ac01865bb03b77a0dee0018
Turtlegods <strollist@gmail.com>
2025-07-23 15:53:07 +0800

    加入memberParams, filter Gender 可以用性別filter，同時Filter 掉自己，match list看不到自己

---
+++ API/Helpers/MemberParams.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace API.Helpers;
+
+public class MemberParams : PagingParams
+{
+    public string? Gender { get; set; }
+    
+    public string? CurrentMemberId { get; set; }
+}
--- API/Interfaces/IMemberRepository.cs
+++ API/Interfaces/IMemberRepository.cs
@@ -10,7 +10,7 @@ public interface IMemberRepository
 {
     void Update(Member user);
     Task<bool> SaveAllAsync();
-    Task<PaginatedResult<Member>> GetMembersAsync(PagingParams pagingParams);
+    Task<PaginatedResult<Member>> GetMembersAsync(MemberParams memberParams);
     Task<Member?> GetMemberByIdAsync(string id);
     Task<IReadOnlyList<Photo>> GetPhotosForMemberAsync(string memberId);
     Task<Member?> GetMemberForUpdate(string id);
