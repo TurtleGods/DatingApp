d4963c9beb32605df384ae1b6cc9e3d97542fe48
Turtlegods <strollist@gmail.com>
2025-08-06 23:05:36 +0800

    sending messages via the hub

--- API/SignalR/MessageHub.cs
+++ API/SignalR/MessageHub.cs
@@ -1,4 +1,7 @@
 using System;
+using API.Data;
+using API.DTOs;
+using API.Entities;
 using API.Extensions;
 using API.Interfaces;
 using Microsoft.AspNetCore.Authorization;
@@ -8,7 +11,7 @@ using Microsoft.Extensions.Primitives;
 namespace API.SignalR;
 
 [Authorize]
-public class MessageHub(IMessageRepository messageRepository) : Hub
+public class MessageHub(IMessageRepository messageRepository, MemberRepository memberRepository) : Hub
 {
     public override async Task OnConnectedAsync()
     {
@@ -18,10 +21,35 @@ public class MessageHub(IMessageRepository messageRepository) : Hub
 
         await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
 
-        var messages = await messageRepository.GetMessageThread(GetUserId(),otherUser);
+        var messages = await messageRepository.GetMessageThread(GetUserId(), otherUser);
         await Clients.Group(groupName).SendAsync("ReceiveMessageThread", messages);
     }
 
+    public async Task SendMessage(CreateMessageDto createMessageDto)
+    {
+        var sender = await memberRepository.GetMemberByIdAsync(GetUserId());
+        var recipient = await memberRepository.GetMemberByIdAsync(createMessageDto.RecipientId);
+
+        if (recipient == null || sender == null || sender.Id == createMessageDto.RecipientId)
+        {
+            throw new HubException("Cannot send Message");
+        }
+
+        var message = new Message
+        {
+            SenderId = sender.Id,
+            RecipientId = recipient.Id,
+            Content = createMessageDto.Content,
+        };
+
+        messageRepository.AddMessage(message);
+        if (await messageRepository.SaveAllAsync())
+        {
+            var group = GetGroupName(sender.Id, recipient.Id);
+            await Clients.Group(group).SendAsync("NewMessage", message.ToDto());
+        };
+
+    }
     public override Task OnDisconnectedAsync(Exception? exception)
     {
         return base.OnDisconnectedAsync(exception);
